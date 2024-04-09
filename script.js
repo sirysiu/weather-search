b1d7968ad70f5e83cf1d53d98e702970c089db4c
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-04-09 16:01:06 -0400

    connected that class name so cards can be added on html file

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-var weather = document.getElementById("weather");
+var weatherReport = document.getElementById("weather-report");
 var forecast = document.getElementById("forecast");
 
+// Function that clears the Weather report when a user selects a new location
+function clearWeatherReport() {
+    weatherReport.innerHTML = "";
+}
+
+// Function that stores the searched locations by the user. 
 function storeSearch(location) {
     var storedLocations = JSON.parse(localStorage.getItem("storedLocations")) || [];
 
@@ -9,8 +15,10 @@ function storeSearch(location) {
     localStorage.setItem("storedLocations", JSON.stringify(storedLocations));
     displaySearchHistory();
 
-} 
+}
 
+// This is the function that gets the Weather Data from the openWeather API. It takes in the Lat and Lon from the storeSearch function, using the geoAPI from openWeather. 
+// It then creates the Weather Report section from that data. 
 function getWeatherApi(lat, lon) {
     var units = "imperial"
     var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=' + units + '&appid=66726d07d717ef16f36bc84905667fa1';
@@ -111,7 +119,13 @@ function getWeatherApi(lat, lon) {
         })
 };
 
+
+// This is the code that runs the search functionality and the search history.
+
 var locationSearchEl = document.querySelector("#search-box");
+
+// When the user searches a location, we will pull data from the weather API about that locations Lat and Lon, so we can run it through the openWeather API. 
+// It also saves this location to local storage.  
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -121,7 +135,7 @@ var formSubmitHandler = function (event) {
     if (locationSearch) {
         storeSearch(locationSearch);
         // This is the code to convert the location name into lon and lat values. 
-        var geoApiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + locationSearch + "&limit=1&appid=7683dd89e3713c696366aefeb8fa991f"
+        var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + locationSearch + "&limit=1&appid=7683dd89e3713c696366aefeb8fa991f"
 
         fetch(geoApiUrl)
             .then(function (response) {
@@ -153,16 +167,97 @@ var formSubmitHandler = function (event) {
 var form = document.querySelector('form');
 form.addEventListener('submit', formSubmitHandler);
 
- 
-function getCityGeoData(city) {
-fetch('https://api.openweathermap.org/geo/1.0/direct?appid=66726d07d717ef16f36bc84905667fa1&limit=1&q=Charlotte')
-    .then(function(response) {
-    return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-        getGeoWeather(data[0].lat, data[0].lon);
-    })
-}
-getCityGeoData();
 
+
+
+
+
+// This is the function that will create the buttons with search history. 
+function displaySearchHistory() {
+    var previousSearch = JSON.parse(localStorage.getItem("storedLocations"));
+    var searchHistory = document.querySelector("#search-history")
+
+    searchHistory.innerHTML = "";
+
+
+    var endIndex = previousSearch.length;
+    var startIndex = Math.max(0, previousSearch.length - 10);
+    var uniqueCities = new Set();
+
+    for (var i = endIndex - 1; i >= startIndex; i--) {
+        var cityName = previousSearch[i];
+        if (cityName && !uniqueCities.has(cityName)) {
+
+            var historyBtn = document.createElement("button");
+            historyBtn.classList.add("prev-results-btn")
+            historyBtn.value = cityName;
+            historyBtn.textContent = cityName;
+            attachHoverEffect(historyBtn);
+
+            
+            historyBtn.addEventListener("click", function (event) {
+
+                var nameValue = event.target.value;
+
+                if (nameValue) {
+
+                    // This is the code to convert the location name into lon and lat values. 
+                    var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + nameValue + "&limit=1&appid=7683dd89e3713c696366aefeb8fa991f"
+
+                    fetch(geoApiUrl)
+                        .then(function (response) {
+                            return response.json()
+                        })
+                        .then(function (data) {
+                            if (data.length > 0) {
+                                var lat = data[0].lat;
+                                var lon = data[0].lon;
+                                // console.log("Latitude:", lat, "Longitude:", lon);
+                                clearWeatherReport();
+                                getWeatherApi(lat, lon);
+                            }
+                            else {
+                                console.log("location not found");
+                            }
+                        })
+                        .catch(function (error) {
+                            console.log("Error fetching data: ", error);
+                        });
+
+
+                } else {
+                    alert('Please try a different location');
+                }
+
+            });
+
+            searchHistory.appendChild(historyBtn);
+            uniqueCities.add(cityName);
+        }
+
+        
+    }
+    
+}
+
+displaySearchHistory();
+
+
+function attachHoverEffect(button) {
+
+    button.addEventListener("mouseover", function () {
+        button.style.boxShadow = "0 8px 13px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19)";
+    })
+    button.addEventListener("mouseout", function () {
+        button.style.boxShadow = "";
+    });
+}
+
+
+// This resets is the code for the reset history button. 
+var resetPress = function resetSearchHistory() {
+    localStorage.removeItem("storedLocations");
+    window.location.reload();
+};
+var clearPress = document.querySelector("#clear");
+clearPress.addEventListener('click', resetPress);
